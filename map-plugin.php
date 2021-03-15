9478b2d52a8d3892623a4a712e38d87dcebbcdda
Haris Muslić <39027423+thewhitewolf2411@users.noreply.github.com>
2021-03-15 15:44:37 +0100

    changes to mobile map

---
+++ map-plugin.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name:     Map Plugin
+ * Plugin URI:      rural.ba
+ * Description:     Interactive map
+ * Author:          Haris Muslic
+ * Text Domain:     map-plugin
+ * Version:         0.1.0
+ * 
+ * @package         Map_plugin
+ */
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+//import React from 'react';
+import React from '../node_modules/react';
+//import ReactDOM from 'react-dom';
+import ReactDOM from '../node_modules/react-dom';
 import App from './Components/App';
 
 import './css/app.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />,document.getElementById('mapcontainer'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
